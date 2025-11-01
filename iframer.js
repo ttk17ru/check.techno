1af85e9bf30fbecf9aeb58601c7feeef7c754fa2
T3CHN0 <electronano285@gmail.com>
2025-11-01 17:33:39 +0200

    Add files via upload

---
+++ iframer.js
@@ -0,0 +1,13 @@
+let frame = document.createElement("iframe");
+frame.src="https://karem-mahmoud.vercel.app";
+document.body.append(frame);
+frame.style.width = "100%";
+frame.style.height = "100%";
+frame.style.borderStyle = "none";
+
+document.body.style.width = "100%";
+document.body.style.height = "100vh";
+document.body.style.overflow = "hidden";
+document.body.style.margin = "0";
+document.body.style.borderStyle = "none";
+document.body.style.backgroundColor = "black";
